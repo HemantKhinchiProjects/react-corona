15471522b0901f61dee090274862d9cac0497096
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-04-27 19:10:41 +0530

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./style.css";
-
+import React from 'react';
+import './style.css';
+import Footer from '../Components/Footer/Footer';
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Footer />
     </div>
   );
 }
