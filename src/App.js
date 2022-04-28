82bfc97a7b1e8db269261a0e134b59a8850f7588
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-04-28 13:55:35 +0530

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './style.css';
 import Footer from './Components/Footer/Footer';
+import TopNavbar from './Components/Navbar/TopNavbar';
 export default function App() {
   return (
     <div className="container-scroller">
-      <h1>Hello StackBlitz!</h1>
-      <Footer />
+      <div className="container-fluid page-body-wrapper">
+        <TopNavbar />
+        <main className="main-panel">
+          <div className="content-wrapper"></div>
+          <Footer />
+        </main>
+      </div>
     </div>
   );
 }
