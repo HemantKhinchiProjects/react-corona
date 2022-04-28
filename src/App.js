ed61743614d80df312e58891227868f07eeea603
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-04-28 12:00:56 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './style.css';
 import Footer from './Components/Footer/Footer';
 export default function App() {
   return (
-    <div>
+    <div className="container-scroller">
       <h1>Hello StackBlitz!</h1>
       <Footer />
     </div>
