bc8fe4ab1c352c5e4b0d23629a1d7cc29d93c232
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-04-29 12:05:53 +0530

    2 files modified, 1 added, 1 deleted

--- src/Components/DashbordMeter/DashbordMeters.js
+++ src/Components/DashbordMeter/DashbordMeters.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Data from './data';
 import Card from './card/Card';
 const DashBordMeters = () => {
   return (
--- src/Components/DashbordMeter/card/Card.js
+++ src/Components/DashbordMeter/card/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Classes from './Card.module.css';
 const Card = () => {
   return (
