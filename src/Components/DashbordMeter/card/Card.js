1dfb0cbf2078a734fc2b92930117e58568ec0a4d
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-04-29 12:40:40 +0530

    1 file modified

--- src/Components/DashbordMeter/card/Card.js
+++ src/Components/DashbordMeter/card/Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import Classes from './Card.module.css';
-const Card = ({ dailyData }) => {
+const Card = ({ dailyDsta }) => {
   return (
-    <>
-      {perform.map((info) => {
+    <div className="row">
+      {dailyDsta.map((info) => {
         const { id, dec, amount, groth } = info;
         return (
           <div class="col-xl-3 col-sm-6 grid-margin stretch-card" key={id}>
@@ -14,9 +14,9 @@ const Card = ({ dailyData }) => {
                   <div class="col-9">
                     <div class="d-flex align-items-center align-self-start">
                       {/* <h3 class="mb-0">$12.34{`${info.amount}`}</h3> */}
-                      <h3 class="mb-0">$12.34{`${info.amount}`}</h3>
+                      <h3 class="mb-0">${amount}</h3>
                       <p class="text-success ml-2 mb-0 font-weight-medium">
-                        +3.5%
+                        +{groth}%
                       </p>
                     </div>
                   </div>
@@ -28,14 +28,13 @@ const Card = ({ dailyData }) => {
                     </div>
                   </div>
                 </div>
-                <h6 class="text-muted font-weight-normal">Potential growth</h6>
+                <h6 class="text-muted font-weight-normal">{dec}</h6>
               </div>
             </div>
           </div>
         );
       })}
-      ;
-    </>
+    </div>
   );
 };
 export default Card;
