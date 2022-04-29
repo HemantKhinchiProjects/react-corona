7f4bfbc20d369e8ad4f2998ccf5a66a2bc056b14
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-04-29 12:03:25 +0530

    1 file modified

--- src/Components/DashbordMeter/card/data.js
+++ src/Components/DashbordMeter/card/data.js
@@ -1,37 +1,26 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    dec: 'Potential growth',
+    amount: 12.34,
+    groth: 3.5,
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    dec: 'Revenue current',
+    amount: 17.34,
+    groth: 11,
   },
   {
     id: 3,
-    name: 'Larry Little',
-    age: 36,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    dec: 'Daily Income',
+    amount: 12.34,
+    groth: 0.5,
   },
   {
     id: 4,
-    name: 'Sean Walsh',
-    age: 34,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-  },
-  {
-    id: 5,
-    name: 'Lola Gardner',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    dec: 'Expense current',
+    amount: 20.34,
+    groth: 15,
   },
 ];
