1c19c5e03bbdc6c16b4fabcb94d27bc28b15660f
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-05-01 18:34:46 +0530

    3 files modified

--- src/Components/openProjeact/OpenProjects.js
+++ src/Components/openProjeact/OpenProjects.js
@@ -2,6 +2,14 @@ import React from 'react';
 import CardStyle from '../ui/CardStyle.js';
 import './openproject.css';
 const OpenProjects = () => {
-  return <CardStyle>Open Projects</CardStyle>;
+  return (
+    <div class="col-md-8 grid-margin stretch-card">
+      <CardStyle>
+        <div className="d-flex flex-row justify-content-between">
+          <h4 className="card-title mb-1">Open Projects</h4>
+        </div>
+      </CardStyle>
+    </div>
+  );
 };
 export default OpenProjects;
