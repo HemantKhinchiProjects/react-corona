e4a669ae1e7e14844230d73844d6eec4d16797e4
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-05-02 16:16:58 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './style.css';
 import Footer from './Components/Footer/Footer';
 import TopNavbar from './Components/Navbar/TopNavbar';
 import SideBar from './Components/Navbar/SideBar';
-import DashBordMeters from './Components/DashbordMeter/DashbordMeters';
-import TransactionHistory from './Components/transactionHistory/TransactionHistory';
-import OpenProjects from './Components/openProjeact/OpenProjects';
-import SalseMetor from './Components/salseMetor/SalseMetor';
+
+
+
+
 
 import Dashbord from './Pages/dashbord/Dashbord';
 import UsePages from './Pages/use-pages/UsePages';
@@ -25,40 +25,35 @@ export default function App() {
       <div className="container-fluid page-body-wrapper">
         <TopNavbar />
         <main className="main-panel">
-          <Route path="/dashbord">
-            <Dashbord />
-          </Route>
-          <Route path="/use-pages">
-            <UsePages />
-          </Route>
-          <Route path="/ui-elements">
-            <UiElement />
-          </Route>
-          <Route path="/pages/charts">
-            <Charts />
-          </Route>
-          <Route path="/pages/Form-element">
-            <FormElement />
-          </Route>
-          <Route path="/pages/icons">
-            <Icons />
-          </Route>
-          <Route path="/pages/document">
-            <Document />
-          </Route>
-          <Route path="/pages/tables">
-            <Tables />
-          </Route>
           <div className="content-wrapper">
-            <DashBordMeters />
-            <div className="row">
-              <TransactionHistory />
-              <OpenProjects />
-            </div>
-            <div className="row">
-              <SalseMetor />
-            </div>
+            <Route path="/dashbord">
+              <Dashbord />
+            </Route>
+            <Route path="/use-pages">
+              <UsePages />
+            </Route>
+            <Route path="/ui-elements">
+              <UiElement />
+            </Route>
+            <Route path="/pages/charts">
+              <Charts />
+            </Route>
+            <Route path="/pages/Form-element">
+              <FormElement />
+            </Route>
+            <Route path="/pages/icons">
+              <Icons />
+            </Route>
+            <Route path="/pages/document">
+              <Document />
+            </Route>
+            <Route path="/pages/tables">
+              <Tables />
+            </Route>
           </div>
+
+          
+         
           <Footer />
         </main>
       </div>
--- src/Pages/dashbord/Dashbord.js
+++ src/Pages/dashbord/Dashbord.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import DashBordMeters from '../../Components/DashbordMeter/DashbordMeters';
+import TransactionHistory from '../../Components/transactionHistory/TransactionHistory';
+import OpenProjects from '../../Components/openProjeact/OpenProjects';
+import SalseMetor from '../../Components/salseMetor/SalseMetor';
 const Dashbord = () => {
-  return <div>Dashbord Component</div>;
+  return (
+    <>
+      <DashBordMeters />
+      <div className="row">
+        <TransactionHistory />
+        <OpenProjects />
+      </div>
+      <div className="row">
+        <SalseMetor />
+      </div>
+    </>
+  );
 };
 export default Dashbord;
